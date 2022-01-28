6e8c6f8706088be4e26769a6cfdf7d27c04a24f8
Dmytro <dim.fed.13@gmail.com>
2022-01-28 16:54:12 +0200

    In class Student created 4 parameters and construction

--- ConsoleApp/Student.cs
+++ ConsoleApp/Student.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    class Student
+    public class Student : Human
     {
+        public int Course { set; get; }
+        public string Group { set; get; }
+        public string Faculty { set; get; }
+        public string NameUnivercity { set; get; }
+        public Student(string firstname, string lastname, DateTime dateOfBirth, int course, string group, string faculty, string nameunivercity) : base(firstname, lastname, dateOfBirth)
+        {
+            Course = course;
+            Group = group;
+            Faculty = faculty;
+            NameUnivercity = nameunivercity;
+        }
     }
+
 }
