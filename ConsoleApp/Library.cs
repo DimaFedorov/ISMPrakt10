fa47aa21ef7cb6b6ef888baaee685c4373aada8b
Dmytro <dim.fed.13@gmail.com>
2022-01-28 17:03:14 +0200

    In class Library created 3 parameters and construction

--- ConsoleApp/Library.cs
+++ ConsoleApp/Library.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    class Library
+    public class Library : Student
     {
+        public int ReadersTicketNumber { set; get; }
+
+        public DateTime DateOfIssue { set; get; }
+        public int MonthlyFee { set; get; }
+
+        public Library(string firstname, string lastname, DateTime dateOfBirth, int course, string group, string faculty, string nameunivercity,
+            int readersticketnumber, DateTime dateTime, int monthlyfee) : base(firstname, lastname, dateOfBirth, course, group, faculty, nameunivercity)
+        {
+            ReadersTicketNumber = readersticketnumber;
+            DateOfIssue = dateTime;
+            MonthlyFee = monthlyfee;
+        }
     }
+
 }
